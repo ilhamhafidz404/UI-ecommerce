56c7cb75e3e576d2ee7c3de69f1adbe2a08fd4fb
ilhamhafidz404 <xxspanzie@gmail.com>
2023-11-01 23:17:20 +0700

    Images and Stack

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,8 @@ class MyApp extends StatelessWidget {
           body: SingleChildScrollView(
         child: Container(
           color: const Color(0xff054456),
-          padding: const EdgeInsets.all(20),
+          padding:
+              const EdgeInsets.only(top: 35, bottom: 20, left: 20, right: 20),
           child: Column(
             children: [
               Row(
@@ -43,7 +44,7 @@ class MyApp extends StatelessWidget {
                     child: const Icon(Icons.search),
                     style: ButtonStyle(
                       padding: MaterialStateProperty.all(const EdgeInsets.only(
-                          top: 20, bottom: 20, left: 10, right: 10)),
+                          top: 15, bottom: 15, left: 5, right: 5)),
                       backgroundColor:
                           MaterialStateProperty.all(const Color(0xfff58827)),
                     ),
@@ -53,51 +54,132 @@ class MyApp extends StatelessWidget {
               const SizedBox(
                 height: 50,
               ),
-              Container(
-                width: 500,
-                height: 200,
-                decoration: const BoxDecoration(
-                    color: Color(0xff1aa0b9),
-                    borderRadius: BorderRadius.all(Radius.circular(20))),
-                child: const Center(
-                  child: Text(
-                    "LAMP",
-                    style: TextStyle(color: Colors.white, fontSize: 30),
+              Stack(
+                children: [
+                  Container(
+                    width: 500,
+                    height: 200,
+                    decoration: const BoxDecoration(
+                        color: Color(0xff1aa0b9),
+                        borderRadius: BorderRadius.all(Radius.circular(20))),
+                    child: const ClipRRect(
+                      borderRadius: BorderRadius.all(Radius.circular(20)),
+                      child: Image(
+                        image: NetworkImage(
+                            "https://fastly.picsum.photos/id/20/3670/2462.jpg?hmac=CmQ0ln-k5ZqkdtLvVO23LjVAEabZQx2wOaT4pyeG10I"),
+                        height: 200,
+                        width: 500,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
                   ),
-                ),
+                  Positioned(
+                      bottom: 0,
+                      left: 0,
+                      right: 0,
+                      child: Container(
+                        padding: const EdgeInsets.all(10),
+                        decoration: const BoxDecoration(
+                            borderRadius: BorderRadius.only(
+                                bottomLeft: Radius.circular(20),
+                                bottomRight: Radius.circular(20)),
+                            color: Colors.white),
+                        child: const Center(
+                          child: Text(
+                            "CATEGORY 1",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 20),
+                          ),
+                        ),
+                      ))
+                ],
               ),
               const SizedBox(
                 height: 25,
               ),
-              Container(
-                width: 500,
-                height: 200,
-                decoration: const BoxDecoration(
-                    color: Color(0xff1aa0b9),
-                    borderRadius: BorderRadius.all(Radius.circular(20))),
-                child: const Center(
-                  child: Text(
-                    "CAR",
-                    style: TextStyle(color: Colors.white, fontSize: 30),
+              Stack(
+                children: [
+                  Container(
+                    width: 500,
+                    height: 200,
+                    decoration: const BoxDecoration(
+                        color: Color(0xff1aa0b9),
+                        borderRadius: BorderRadius.all(Radius.circular(20))),
+                    child: const ClipRRect(
+                      borderRadius: BorderRadius.all(Radius.circular(20)),
+                      child: Image(
+                        image: NetworkImage(
+                            "https://fastly.picsum.photos/id/71/5000/3333.jpg?hmac=wBjyqoAke0uv6bTtbbIby9s-VTQ52gIkI-QVXWS3W0I"),
+                        height: 200,
+                        width: 500,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
                   ),
-                ),
+                  Positioned(
+                      bottom: 0,
+                      left: 0,
+                      right: 0,
+                      child: Container(
+                        padding: const EdgeInsets.all(10),
+                        decoration: const BoxDecoration(
+                            borderRadius: BorderRadius.only(
+                                bottomLeft: Radius.circular(20),
+                                bottomRight: Radius.circular(20)),
+                            color: Colors.white),
+                        child: const Center(
+                          child: Text(
+                            "CATEGORY 2",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 20),
+                          ),
+                        ),
+                      ))
+                ],
               ),
               const SizedBox(
                 height: 25,
               ),
-              Container(
-                width: 500,
-                height: 200,
-                decoration: const BoxDecoration(
-                    color: Color(0xff1aa0b9),
-                    borderRadius: BorderRadius.all(Radius.circular(20))),
-                child: const Center(
-                  child: Text(
-                    "PLANT",
-                    style: TextStyle(color: Colors.white, fontSize: 30),
+              Stack(
+                children: [
+                  Container(
+                    width: 500,
+                    height: 200,
+                    decoration: const BoxDecoration(
+                        color: Color(0xff1aa0b9),
+                        borderRadius: BorderRadius.all(Radius.circular(20))),
+                    child: const ClipRRect(
+                      borderRadius: BorderRadius.all(Radius.circular(20)),
+                      child: Image(
+                        image: NetworkImage(
+                            "https://fastly.picsum.photos/id/42/3456/2304.jpg?hmac=dhQvd1Qp19zg26MEwYMnfz34eLnGv8meGk_lFNAJR3g"),
+                        height: 200,
+                        width: 500,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
                   ),
-                ),
-              )
+                  Positioned(
+                      bottom: 0,
+                      left: 0,
+                      right: 0,
+                      child: Container(
+                        padding: const EdgeInsets.all(10),
+                        decoration: const BoxDecoration(
+                            borderRadius: BorderRadius.only(
+                                bottomLeft: Radius.circular(20),
+                                bottomRight: Radius.circular(20)),
+                            color: Colors.white),
+                        child: const Center(
+                          child: Text(
+                            "CATEGORY 3",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 20),
+                          ),
+                        ),
+                      ))
+                ],
+              ),
             ],
           ),
         ),
